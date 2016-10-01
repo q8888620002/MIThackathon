c58f681f38484c1432e689c304faec9bfdf2e3ca
absolutelyNoWarranty <computerholic5568@gmail.com>
2016-10-01 19:39:28 +0800

    Add patients and patient_info pages

--- app/main/patient.py
+++ app/main/patient.py
@@ -1,4 +1,5 @@
 import os
+import json
 import cPickle as pickle
 
 
@@ -8,14 +9,27 @@ if not os.path.exists(DATA_STORE_DIR):
 print DATA_STORE_DIR
 
 
-def get_patient(_id=None):
+def get_patient(_id):
     # Try to restore saved
     p = Patient(_id)
+    try:
+        p.restore()
+    except IOError as e:
+        print e
+    return p
 
+def get_all_patients():
+    res = []
+    for _id in os.listdir(DATA_STORE_DIR):
+        res.append(get_patient(_id))
+    return res
 
 class Patient(object):
     '''A class representing a patient
     '''
+
+    attributes = ['name', 'gender', 'age', '_diagnosis',
+                  'location', '_location']
     def __init__(self, _id):
         # Unique id (NHS id)
         self._id = _id
@@ -27,10 +41,8 @@ class Patient(object):
         # Age 
         self.age = None
 
-        # List of symptoms (infermedica symptom ids)
-        self._symptoms = None
-        # List of conditions (infermedica condition ids)
-        self._conditions = None
+        # Infermedica diagnosis object as dict
+        self._diagnosis = None
 
         # User provided location
         self.location = None
@@ -50,6 +62,11 @@ class Patient(object):
         with open(self._chatlog_path, 'a+') as f:
             f.write("{}\n".format(text))
             
+    def get_chatlog(self):
+        with open(self._chatlog_path, 'a+') as f:
+            text = f.read()
+        return text
+
     @property
     def _savepath(self):
         path = os.path.join(DATA_STORE_DIR, self._id, 'obj')
@@ -59,6 +76,15 @@ class Patient(object):
             pass
         return path
     
+    def restore(self):
+        with open(self._savepath, 'rb') as f:
+            d = json.load(f)
+
+        for a in self.attributes:
+            setattr(self, a, d[a])
+
     def save(self):
+        d = {a: getattr(self, a) for a in self.attributes}
+
         with open(self._savepath, 'wb') as f:
-            pickle.dump(self, f)
+            json.dump(d, f)
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
 from flask import session, redirect, url_for, render_template, request
 from . import main
 from .forms import LoginForm
+from .patient import get_patient, get_all_patients
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -26,3 +27,19 @@ def chat():
     if name == '' or room == '':
         return redirect(url_for('.index'))
     return render_template('chat.html', name=name, room=room)
+
+
+@main.route('/patients')
+def patients():
+    """Show list of patients
+    """
+    return render_template('patients.html', patients=get_all_patients())
+
+@main.route('/patient_info/<_id>')
+def patient_info(_id):
+    """Show recorded patient info and chat."""
+    p = get_patient(_id)
+    print _id
+    print p.name
+    chat_log = p.get_chatlog()
+    return render_template('patient_info.html', patient=p, chat_log=chat_log)
